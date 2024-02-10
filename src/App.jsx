1cfc749cf6fffea77883a00b4715dc093d494584
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2024-02-10 21:09:06 +0600

    Deleted banner, make it user friendly and made it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Home from "./pages/components-overview/Home/Home";
+import Home from "./pages/components-overview/home/home";
 
 function App() {
   return (
-    <div className="overflow-hidden">
+    <div className="w-full h-[100vh] bg-gradient-to-r  from-indigo-500 to-pink-500 via-purple-500">
       <Home />
     </div>
   );
--- src/pages/components-overview/Home/Home.jsx
+++ src/pages/components-overview/Home/Home.jsx
@@ -1,30 +1,11 @@
 import React from "react";
-import Banner from "../../../Components/UI/shared/Banner";
 import Todo from "../Todo/Todo";
 
 export default function Home() {
   return (
-    <div className="carousel h-[100vh]">
-      <div id="slide1" className="carousel-item relative w-full bg-[#696a6f]">
-        <div className="w-[100vw]">
-          <Banner />
-        </div>
-      </div>
-      <div
-        id="slide2"
-        className="carousel-item relative w-full opacity-100 bg-gradient-to-r  from-indigo-500 to-pink-500 via-purple-500"
-      >
-        <div>
-          <Todo />
-        </div>
-        {/* <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <a href="#slide1" className="btn btn-circle">
-            ❮
-          </a>
-          <a href="#slide3" className="btn btn-circle">
-            ❯
-          </a>
-        </div> */}
+    <div className="w-full h-[100vh] bg-gradient-to-r  from-indigo-500 to-pink-500 via-purple-500 overflow-hidden max-[600px]:overflow-x-scroll">
+      <div>
+        <Todo />
       </div>
     </div>
   );
